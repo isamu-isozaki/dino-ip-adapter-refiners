ba3020f4f6ccb6904f314d6331196f2979111b13
isamu-isozaki <isamu.website@gmail.com>
2024-04-02 15:25:39 -0400

    Logging

--- src/dino_ip_adapter_refiners/trainer.py
+++ src/dino_ip_adapter_refiners/trainer.py
@@ -193,7 +193,7 @@ class BaseTrainer(
                 cond_image,
                 mean=[0.48145466, 0.4578275, 0.40821073],
                 std=[0.26862954, 0.26130258, 0.27577711],
-            )
+            )[None]
             print(cond_image.shape)
             image_embedding = image_encoder(cond_image)
             image_embedding = self.image_proj(cond_image)

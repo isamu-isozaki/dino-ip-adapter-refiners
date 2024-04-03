daef09b91e6d57079c085ee8ff4c1d940634d555
isamu-isozaki <isamu.website@gmail.com>
2024-04-03 17:49:31 -0400

    Removed expanded dim

--- src/dino_ip_adapter_refiners/trainer.py
+++ src/dino_ip_adapter_refiners/trainer.py
@@ -183,7 +183,7 @@ class BaseTrainer(
                 cond_image,
                 mean=[0.48145466, 0.4578275, 0.40821073],
                 std=[0.26862954, 0.26130258, 0.27577711],
-            )[None]
+            )
             image_embedding = image_encoder(cond_image)
             image_embedding = self.image_proj(image_embedding)
             negative_image_embedding = self.black_image_embedding
